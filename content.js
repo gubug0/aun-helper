54ec03707878b80ced6f01077e47b092f1189aaf
Huiseung Jeon <gubug@gubug.kr>
2022-03-29 11:28:32 +0900

    0.0.1 first version

---
+++ content.js
@@ -0,0 +1,10 @@
+function injectJs(srcFile) {
+    var scr = document.createElement('script');
+    scr.src = srcFile;
+	scr.type = "text/javascript";
+    document.getElementsByTagName('head')[0].appendChild(scr);
+}
+
+$(document).ready(function() {
+	injectJs(chrome.extension.getURL('inject.js'));
+});
