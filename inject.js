a44c9fbb45f482d37834373c86e4818e7cc52825
Huiseung Jeon <gubug@gubug.kr>
2022-03-29 19:15:11 +0900

    5초사냥 사용자 대응 추가

--- inject.js
+++ inject.js
@@ -11,7 +11,6 @@
 		return lastBattleTime;
 	}
 
-
 	const findBattleButton = function() {
 		if (document.querySelector("form[action=bt]")) {
 			return document.querySelector("form[action=bt]");
@@ -26,6 +25,10 @@
 		const currentTime = new Date()
 		localStorage.setItem("lastBattle", currentTime.getTime());
 	}
+	
+	const is5SecondsBattleUser = function() {
+		return Array.from(document.querySelectorAll(".offer.offer-radius.table font")).filter(item => item.textContent.includes("5초사냥 사용자, 보너스 ON")).length > 0
+	}
 
 	var callCount = 0
 	const timerRun = function() {
@@ -36,14 +39,20 @@
 		var lastBattleTime = getLastBattleTime();
 		var currentTime = new Date();
 		
-		if (currentTime.getTime() - lastBattleTime.getTime() >= 9500) {
+		const fiveSecondsBattle = is5SecondsBattleUser();
+		
+		const battleDuration = fiveSecondsBattle ? 4500 : 9500;		
+		if (currentTime.getTime() - lastBattleTime.getTime() >= battleDuration) {
+			if (fiveSecondsBattle) {
+				console.log("5초사냥 유저입니다.");
+			}
 			console.log("화면에서는 " + document.querySelector("#reload .hanna b").textContent + "초가 남았다고 하네요.");
 			console.log("" + ((currentTime.getTime() - lastBattleTime.getTime())/1000) + "초만에 사냥을 합니다.");
 			console.log("사냥을 시작합니다.");
 			submitBattle();
 			document.querySelector("form[action=bt]").submit()
 		} else {
-			if (callCount % 100 == 0) {
+			if (callCount % 50 == 0) {
 				console.log("화면에서는 " + document.querySelector("#reload .hanna b").textContent + "초가 남았다고 하네요.");
 				console.log("최근 사냥부터 " + ((currentTime.getTime() - lastBattleTime.getTime())/1000) + "초가 지났습니다.");
 			}
@@ -60,7 +69,6 @@
 		
 	var battleButton = findBattleButton();
 	if (battleButton) {
-		console.log("add listener.");
 		battleButton.addEventListener("submit", function() {
 			submitBattle();
 		});
