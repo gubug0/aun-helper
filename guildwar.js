cd16893092f7091d0f903b6e44fe4b9e5c8cb23d
gubug <gubug@gubug.kr>
2022-05-18 09:52:03 +0900

    길드전 실패 케이스 대응 추가

--- guildwar.js
+++ guildwar.js
@@ -20,14 +20,20 @@ function monitorGuildWar() {
 			return;
 		}
 		const errorMessage = errorMessageDom.textContent.replaceAll("\n", "");
-		console.log(errorMessage);
-		const remainSecond = errorMessage.replace(/.*도시 공격까지 ([0-9]+) 초 기다려 주세요..*/, "$1")
-		if (!remainSecond) {
-			return;
+		if (errorMessage.match(/.*도시 공격까지 ([0-9]+) 초 기다려 주세요..*/)) {
+			const remainSecond = errorMessage.replace(/.*도시 공격까지 ([0-9]+) 초 기다려 주세요..*/, "$1")
+			if (!remainSecond) {
+				return;
+			}
+			
+			console.log(remainSecond);
+			setGuildWarTimeAndResetAlarm(new Date().getTime() - (1000 * 60 * 10 - (parseInt(remainSecond, 10) * 1000) - 1000));
+		} else if (errorMessage.match(/.*공격할 수 있는 몸상태가 아닙니다. 여인숙에 가서 건강도를 회복하십시오.*/)) {
+			addLog("HP부족으로 길드전실패");
+			setGuildWarTimeAndResetAlarm(new Date().getTime());
 		}
 		
-		console.log(remainSecond);
-		setGuildWarTimeAndResetAlarm(new Date().getTime() - (1000 * 60 * 10 - (parseInt(remainSecond, 10) * 1000) - 1000));
+		
 		
 	}
 	
