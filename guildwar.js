6b7609506c64ac244ff4b4af80262122067f0018
LimeCake <limecake23@gmail.com>
2022-06-07 16:02:44 +0900

    길드전시 소지금이 일정금액 이상일때 안내추가 (#11)
    Co-authored-by: gubug <gubug@gubug.kr>

--- guildwar.js
+++ guildwar.js
@@ -10,10 +10,30 @@ function monitorGuildWar() {
 	}
 	
 	const guildwarDom = document.querySelector(".glyphicon.glyphicon-tower");
-	
+	var guildTargetSelector = document.querySelector("select[name='tid']");
+	if (!guildTargetSelector && document.querySelector("frame[name='mainFrame']")) guildTargetSelector = document.querySelector("frame[name='mainFrame']").contentWindow.document.querySelector("select[name='tid']");
+
 	if (guildwarDom) {
 		setGuildWarTimeAndResetAlarm(new Date().getTime());
 		return;
+	} else if (guildTargetSelector) { // ATTACK READY PAGE
+		var currentMoneyHolder = document.querySelector("font[color='#DAA520']");
+		if (!currentMoneyHolder) return;
+		var contentHolder = document.querySelector("td[colspan='2']");
+		if (!contentHolder) return;
+		
+		try {
+			const currentMoneyText = currentMoneyHolder.textContent.replace(/.*소지금: ([0-9,]+)골드.*/, '$1').replaceAll(',', '');
+			const currentMoney = parseInt(currentMoneyText);
+			if (currentMoney > 100000) {
+				var warningTextHolder = document.createElement("p");
+				warningTextHolder.style = 'font-style: italic;color: #ff0000;font-weight: bold;font-size: large';
+				warningTextHolder.innerText = "주머니에 골드가 좀 있는데 정말 괜찮으시겠어요?";
+				contentHolder.prepend(warningTextHolder);
+			}
+		} catch (e) {
+			console.log(e);
+		}
 	} else {
 		const errorMessageDom = document.querySelector(".msg.msg-warning.msg-danger-text");
 		if (!errorMessageDom) {
